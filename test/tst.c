d58d0a1cbd35ba76075fe313fdfb1394078b7cae
zouheirrhoumri <zouheir.rhoumri@gmail.com>
2023-09-13 17:10:46 +0100

    add test

---
+++ test/tst.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x ;
+
+    printf("entrez la valeur de x : ");
+    scanf("%d", &x);
+
+    if (x == 3)
+    {
+        printf("that's true");
+    }
+    else{
+        printf("that's false");
+    }
+      
+         return 0;
+}
